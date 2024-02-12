747d26f1855c8652f9c47e572c6a8c77e7ce38dd
Mario <malux97@hotmail.com>
2024-02-11 23:27:45 -0600

    Commit inicial

---
+++ android/app/src/main/kotlin/com/example/contador_2024/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contador_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Has presionado el botón tantas veces:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'Reiniciar',
+            child: const Icon(Icons.refresh),
+          ),
+          const SizedBox(height: 16), // Separación entre los botones
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Menos',
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(height: 16), // Separación entre los botones
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Incrementar',
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
